8cf190353c166f0dcc9f6206eadad066a004d4fc
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-11-23 09:54:17 +0100

    mistake fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <model-viewer id="viewer" src={test} shadow-intensity="1" ar ar-modes="webxr scene-viewer quick-look" camera-controls min-camera-orbit="auto auto 100%" max-camera-orbit="auto auto 100%" min-field-of-view="45deg" max-field-of-view="45deg" environment-image="neutral" auto-rotate camera-controls></model-viewer>
+      <model-viewer id="viewer" src={test} shadow-intensity="1" ar ar-modes="webxr scene-viewer quick-look" camera-controls min-camera-orbit="auto auto 100%" max-camera-orbit="auto auto 100%" min-field-of-view="45deg" max-field-of-view="45deg" environment-image="neutral" auto-rotate ></model-viewer>
     </div>
   );
 }
