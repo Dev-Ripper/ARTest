1b97f15c99581c5fa42d3ff98cb47c7f236a910b
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-11-23 14:55:23 +0100

    added stex logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "@google/model-viewer";
 import test from './assets/test.glb';
+import stex from './assets/stex.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img id="logostex" src={stex} alt="" />
       <model-viewer id="viewer" src={test} shadow-intensity="1" ar ar-modes="webxr scene-viewer quick-look" camera-controls min-camera-orbit="auto auto 100%" max-camera-orbit="auto auto 100%" min-field-of-view="45deg" max-field-of-view="45deg" environment-image="neutral" auto-rotate ></model-viewer>
     </div>
   );
